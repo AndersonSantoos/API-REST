bbca7b11e7dd356cb7e7517455f518f0f946f8e1
Anderson Santos <110641610+DevAndersonSantos@users.noreply.github.com>
2024-04-22 10:10:18 -0300

    Config init.

---
+++ src/database/dbConfig.ts
@@ -0,0 +1,22 @@
+import logger from '../logger'
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const sequelize = new Sequelize({
+    dialect: 'mysql',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
+
+sequelize.authenticate()
+.then(() => {
+    logger.info('Connected to the database successfully.');
+})
+.catch(err => {
+    logger.error('Unable to connect to the database');
+});
+
+export { sequelize };
